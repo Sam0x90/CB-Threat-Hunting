f64081a09406be9260db791524b492b38335a9ce
Sam0x90 <sam0x90@outlook.com>
2023-12-19 18:34:57 +0400

    update script to support action trigger checkboxes

--- Scripts/watchlist_create.py
+++ Scripts/watchlist_create.py
@@ -40,23 +40,40 @@ for filename in os.listdir(watchlist_dir):
                     'name': watchlist_data['name'],
                     'search_query': watchlist_data['search_query'],
                     'index_type': watchlist_data['index_type'],
-                    'description': '{}\nReferences: {}\nTags: {}\nOn Hit: {}'.format(
+                    'description': '{}\nReferences: {}\nTags: {}'.format(
                         watchlist_data.get('description', ''),
                         watchlist_data.get('references', ''),
-                        watchlist_data.get('tags', ''),
-                        watchlist_data.get('on_hit', '')
+                        watchlist_data.get('tags', '')
                     )
                 }
 
                 # POST request to create the watchlist
-                response = requests.post(api_endpoint, headers=headers, data=json.dumps(data), verify=verify_tls)
+                response1 = requests.post(api_endpoint, headers=headers, data=json.dumps(data), verify=verify_tls)
 
                 # Check the response
-                if response.status_code == 200:
+                if response1.status_code == 200:
                     print(f"Watchlist '{watchlist_data['name']}' created successfully")
+                    # Add action trigger based on the "on_hit" field [email , syslog, alert]
+                    on_hit=watchlist_data.get('on_hit', '').split(',')
+                    if on_hit:
+                        # Get the ID of the watchlist to update with action trigger
+                        watchList_id = response1.json()['id']
+                        for action in on_hit:
+                            # Prepare the API endpoint with watchlist ID and action_type
+                            action_url = '{0}/{1}/action_type/{2}'.format(api_endpoint,watchList_id,action.strip())
+                            action_payload = json.dumps({"enabled": True})
+
+                            # PUT request to update the watchlist action trigger
+                            response2 = requests.put(url=action_url, headers=headers, data=action_payload, verify=False)
+                            if response1.status_code == 200:
+                                print(f"Watchlist '{watchList_id}' updated successfully")
+                            else:
+                                print(f"Failed to update action trigger for watchlist ID: '{watchList_id}' with response code: {response1.status_code}")
+                                print(response1.text)
+
                 else:
-                    print(f"Failed to create watchlist '{watchlist_data['name']}': {response.status_code}")
-                    print(response.text)
+                    print(f"Failed to create watchlist '{watchlist_data['name']}': {response1.status_code}")
+                    print(response1.text)
 
             except yaml.YAMLError as exc:
                 print(f"Error parsing YAML file {filename}: {exc}")
